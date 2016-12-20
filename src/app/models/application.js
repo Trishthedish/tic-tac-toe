300b5b26565e9a180c5cf37acb8509e81a8be160
Trish Griego <pmgriego@gmail.com>
2016-12-19 23:03:02 -0800

    Boxes are changing collors. I can't see any visible error messages but I'm unable to get any of my click events to work.

--- src/app/models/application.js
+++ src/app/models/application.js
@@ -1,9 +1,9 @@
 // application.js
 // do we need this??
-import Backbone from 'backbone';
+import Backbone from 'Backbone';
 
 import Game from 'app/models/game';
-import GameView from 'app/views/gameview';
+import GameView from 'app/views/game_view';
 
 const Application = Backbone.Model.extend({
 
--- src/app/views/game_view.js
+++ src/app/views/game_view.js
@@ -1,3 +1,4 @@
+// game_view.js
 import $ from 'jquery';
 import Backbone from 'Backbone';
 import _ from 'underscore';
@@ -8,57 +9,86 @@ import Game from 'app/models/game';
 // import BoardView from 'app/views/board_view';
 import BoardView from 'app/views/board_view';
 
+import Application from 'app/models/application';
+
 const GameView = Backbone.View.extend({
-  initialize: function(options) {
+  initialize: function() {
     console.log("hello!!!");
-    console.log("options >>", options);
+    // console.log("options >>", options);
     // Compile a template to be shared between the individual players?
-    this.playerTemplate = _.template($('#tmpl-player-card').html());
-
-    //keep track of the <ul> element
-    this.listElement = this.$('#player-cards');
-    // keep track of data being put into form by user.
-
-    this.input = {
-      nameX: this.$('.new-player input[nameX="nameX"]'),
-      nameO: this.$('.new-player input[name="nameO"]'),
-    };
-
+    // this.playerTemplate = _.template($('#tmpl-player-card').html());
+    //
+    // //keep track of the <ul> element
+    // this.listElement = this.$('#player-cards');
+    // // keep track of data being put into form by user.
+  console.log("gamveiew >>>", this.model);
+    this.listenTo(this.model, "change", this.render);
+        // console.log(">>> BREADCRUMBS: 0.5 init GameView");
+    // this.input = {
+    //   nameX: this.$('.new-player input[nameX="nameX"]'),
+    //   nameO: this.$('.new-player input[name="nameO"]'),
+    // };
+    this.render();
   }, // end of initialize
 
-  render: function(){
-    console.log('in gameview, rendering game!');
-    this.listElement.empty();
-
+  render: function() {
+    this.delegateEvents();
+    return this;
   },
+  // render: function(){
+  //   console.log('in gameview, rendering game!');
+  //   this.listElement.empty();
+  //
+  // },
 
   events: {
-    'click .btn-save' : 'addPlayer',
-
-    'click .btn-cancel' : 'clearInput',
-
-    'click .btn' : 'sayHello'
+    // 'click .btn-save' : 'addPlayer',
+    'click .gameboard td' : 'makeMove',
+    'click .new-session' : 'newSession',
+    'click .btn-cancel' : 'clearInput'
+    // 'click .btn' : 'sayHello',
   },
 
   sayHello: function() {
     console.log('Hellooooooooooo Friend!');
   },
 
-  addPlayer: function(event){
+  makeMove: function(event) {
     event.preventDefault();
+    this.trigger('select', this);
 
-    // Create a new model from the form data
-    var player = new Player(this.getInput());
+    console.log("you clicked a box");
+    console.log("event target id >>",$(event.target).attr('id'));
+    console.log("The current letter is", this.model.attributes);
 
-    // Create a view around the task and add it to our list
-    var gameView = new GameView({
-      model: player,
-      template: this.playerTemplate
-  });
-  // missing info here.
-  // this.push(gameView);
-  this.model.add(player);
-  this.clearInput();
+    this.listElement = $(event.target);
+// no idea what this is doing. 
+  var symbol = this.model.attributes.symbol;
+
+   this.listElement.html(symbol);
+   this.model.play($(event.target).attr('id'));
+
+  },
+//   addPlayer: function(event){
+//     event.preventDefault();
+//
+//     // Create a new model from the form data
+//     var player = new Player(this.getInput());
+//
+//     // Create a view around the task and add it to our list
+//     var gameView = new GameView({
+//       model: player,
+//       template: this.playerTemplate
+//   });
+//   // missing info here.
+//   // this.push(gameView);
+//   this.model.add(player);
+//   this.clearInput();
+//
+// },
+
+newSession: function(event) {
+  console.log("I tried to make a new session");
 
 },
 
